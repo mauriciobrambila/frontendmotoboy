1c42b90fd166333b3c2fd6196046d58498765cf5
mauriciobrambila <mauriciobrambila2015@gmail.com>
2023-09-13 15:41:31 -0300

    fix: para ps

--- src/formularios/FormEntregas.jsx
+++ src/formularios/FormEntregas.jsx
@@ -26,7 +26,7 @@ export default function FormEntregas(props) {
         if (form.checkValidity()) {
 
             if (!props.modoEdicao) {
-                fetch(urlBase + "http://129.146.68.51/aluno45-pfsii/entrega", {
+                fetch(urlBase + "https://129.146.68.51/aluno45-pfsii/entrega", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify(entrega)
