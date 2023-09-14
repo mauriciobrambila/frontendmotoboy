ce37891bedd3b3771b2ec94081a87c1df234b4d6
mauriciobrambila <mauriciobrambila2015@gmail.com>
2023-09-14 04:25:25 -0300

    fix: fetch

--- src/templates/Pagina.js
+++ src/templates/Pagina.js
@@ -12,7 +12,7 @@ export default function Pagina(props) {
         const [listaMotos, setListaMotos] = useState([]);
 
         useEffect(() => {
-            fetch("http://129.146.68.51/aluno45-pfsii/motos", { method: "GET" })
+            fetch("http://129.146.68.51/aluno45-pfsii/entrega", { method: "GET" })
                 .then((resposta) => {
                     return resposta.json();
                 }).then((dados) => {
