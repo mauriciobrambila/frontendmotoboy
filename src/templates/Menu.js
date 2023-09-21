74db199d3a384c28ed1fa3326b90acf076286d7d
mauriciobrambila <mauriciobrambila2015@gmail.com>
2023-09-20 23:06:06 -0300

    fix: modificando menu.js

--- src/templates/Menu.js
+++ src/templates/Menu.js
@@ -1,4 +1,4 @@
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Container, Navbar, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import React from "react";
@@ -15,14 +15,12 @@ export default function Menu(props) {
     return (
         <Navbar style={navbarStyle} bg="black" variant="dark" expand="lg">
             <Container >
-                <LinkContainer to="/" ><Navbar.Brand><font color="RED"><strong>HOME</strong></font></Navbar.Brand></LinkContainer>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav><font color="black">------------------------------------------------------</font></Nav>
-                <NavbarCollapse><LinkContainer to="/cadastromotoboy"><NavDropdown.Item><strong><font color="white">Motoboy</font></strong></NavDropdown.Item></LinkContainer></NavbarCollapse>
-                <NavbarCollapse><LinkContainer to="/cadastroentrega"><NavDropdown.Item><strong><font color="white">Entregas</font></strong></NavDropdown.Item></LinkContainer></NavbarCollapse>
-
-                    </Navbar.Collapse>
+                           
+            <NavbarCollapse><LinkContainer to="/cadastromotoboy"><NavDropdown.Item><strong><font color="white">Motoboy</font></strong>
+                </NavDropdown.Item></LinkContainer></NavbarCollapse>
+            <NavbarCollapse><LinkContainer to="/cadastroentrega"><NavDropdown.Item><strong><font color="white">Entregas</font></strong>
+                </NavDropdown.Item></LinkContainer></NavbarCollapse>
+                  
             </Container>
         </Navbar>
     );
